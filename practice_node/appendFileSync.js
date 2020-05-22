60ac88d6245b64c83bff0728d05cac7f30477d11
Joseph Kligel <jkligel@gmail.com>
2020-05-21 22:44:01 -0500

    Using bash script to push all updates

---
+++ practice_node/appendFileSync.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+
+fs.appendFileSync('Sample.txt', '\nThis is a new line. Let\'s where it ends up.\n')
---
+++ practice_node/fileWriteSync.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.writeFileSync('Sample.txt', 'This is a sample text to demonstrate the task of writing to files in Nodejs.')
