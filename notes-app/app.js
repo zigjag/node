129e7a5091f177384d0cc752a91e3edcab596f4c
Joseph Kligel <jkligel@gmail.com>
2020-05-25 01:42:16 -0500

    Using bash script to push all updates

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,39 @@
-const validator = require('validator')
+#! /usr/local/bin/node
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
+const yargs = require('yargs')
 
-const msg = getNotes()
-console.log(msg)
+//Customize yarg version
+yargs.version('1.1.0')
 
-console.log(validator.isURL('https:mead.io'))
+//Create add command
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	handler: function(){
+	console.log('Adding a new note!')
+	}
+})
 
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function(){
+		console.log('Removing a note!')
+	}
+})
+
+yargs.command({
+	command: 'list',
+	describe: 'Describes a note',
+	handler: () => console.log('Describing a note!')
+})
+
+yargs.command({
+	command: 'read',
+	describe: 'Reads a note',
+	handler: () => console.log('Reading a note!')
+})
+
+console.log(yargs.argv)
 
